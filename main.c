30ce046acaa82e151ea1f2e10a92e7a52fcbef4b
Danylo Tulainov <niltul22@gmail.com>
2024-02-06 09:30:08 +0100

    Task 2 done

--- main.c
+++ main.c
@@ -4,26 +4,30 @@
 
 // TODO: figure out how to fill fields with random numbers and make them unique for every column and line.
 
+//function gets user's name
 void getUserName() {
 
     printf("Please enter your name:\n");
-
     char name[50];
 
     scanf_s("%s", name);
 
     printf("Welcome %s, let's play sudoku!\n", name);
+
 }
 
+//function to print sudoku fields
 void printFields(int sudokuField[9][9]) {
+
     int y = 1;
-    // find the algorithm for printing letters
-    printf("  A B C D E F G H I \n");
-    printf("  -----------------  \n");
+
+    printf("   ABC DEF GHI\n");
+
     for (int i = 0; i < 9; i++) {
 
-
-//      TODO:   ADD '---' between every field, except the last one.
+        if ((i + 1) % 3 == 1 || i == 0) {
+            printf("   --- --- ---\n");
+        }
 
         printf("%d ",y++);
 
@@ -32,7 +36,8 @@ void printFields(int sudokuField[9][9]) {
             if ((j + 1) % 3 == 1 || j == 0) {
                 printf("%c", '|');
             }
-            printf(" %c ", sudokuField[i][j]);
+
+            printf("%c", sudokuField[i][j]);
 
             if (j == 8) {
                 printf("%c", '|');
@@ -43,21 +48,55 @@ void printFields(int sudokuField[9][9]) {
     }
 }
 
+// function to feel the fields with numbers
+void fillingFieldsWithNumbers(int sudokuField[9][9]) {
+
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            sudokuField[i][j] = '*';
+        }
+    }
+
+    while (1) {
+        printf("\nGive the 'x' (-1 to stop the loop): ");
+        int x = 0;
+        scanf_s("%d", &x);
+        if (x == -1) break;
+
+        printf("\nGive the 'y': ");
+        int y = 0;
+        scanf_s("%d", &y);
+
+        printf("\nWhich value you want to give to this number (from 1 to 9):");
+        int number = 0;
+        scanf_s("%d", &number);
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                sudokuField[x][y] = number + '0';
+            }
+        }
+        printFields(sudokuField);
+
+
+    }
+}
+
 int main() {
 
     getUserName();
 
     int sudoku[9][9];
 
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            sudoku[i][j] = '*';
-        }
-    }
+    printf("Fill the field with the numbers you want. Simply give the coordinates.");
 
-    printFields(sudoku);
+    fillingFieldsWithNumbers(sudoku);
 
     return 0;
 }
 
 
+//Question: how to get the name and surname?
+//Question: exception handling in task 2 - 'which number you want to give'
+//Question: create other C-files and add methods there? import statement?
+//Question: make the numbers from 1 to 10
